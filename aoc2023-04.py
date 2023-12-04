150b0ce83459169e6003ce03546d8d4f66ec6fa9
annaoskarson <anna.oskarson@gmail.com>
2023-12-04 15:27:30 +0100

    Wanted to test collection.Counter(), so I did that.

--- aoc2023-04.py
+++ aoc2023-04.py
@@ -1,22 +1,18 @@
+import collections
 cards = [r.strip('\n') for r in open('aoc2023-04-input.txt')]
 
 ans1 = 0
-amount = {} # Holds the amount of each card.
+amount = collections.Counter() # Holds the amount of each card.
 
 for card in cards:
     n = int(card.split(':')[0].split(' ')[-1])
-    hand = set([int(x) for x in card.split(':')[1].split('|')[0].strip().split(' ') if len(x) > 0])
-    win = set([int(x) for x in card.split(':')[1].split('|')[1].strip().split(' ') if len(x) > 0])
+    hand = set([int(x) for x in card.split(':')[1].split('|')[0].strip().split()])
+    win = set([int(x) for x in card.split(':')[1].split('|')[1].strip().split()])
 
     points = len(hand & win)
 
-    # Look at this card and add it to the stash.
-    if n not in amount.keys():
-        amount[n] = 1
-    else:
-        amount[n] += 1
- 
-    have = amount[n] #The amount we have of this card.
+    amount[n] += 1 # Look at this card and add it to the stash.   
+    have = amount[n] # The amount we have of this card.
 
     if points > 0: # If we win, do the thing
         # For part 1, count the points.
@@ -25,15 +21,9 @@ for card in cards:
         # For part 2, add more scratchcards in the stash, 
         # the amount for each card ahead is defined from how many we have of the current card.
         for i in range(n+1, n+1+points):
-            if i not in amount.keys():
-                amount[i] = have
-            else:
-                amount[i] += have
+            amount[i] += have
 
-
-ans2 = 0
-for this in amount.keys():
-    ans2 = ans2 + amount[this]
+ans2 = amount.total()
 
 print('Day 4, part 1:', ans1)
 print('Day 4, part 2:', ans2)
